0dc62d82702636b5237bb52576048394de0df5ff
David Behar <david@behar.io>
2020-04-10 17:10:20 -0500

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
-
+import localeEs from '@angular/common/locales/es-MX';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component'
 import { TestComponent } from './components/test/test.component';
+import { CartComponent } from './components/cart/cart.component';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    TestComponent
+    TestComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es-mx' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -13,7 +13,8 @@ import { Product } from '../../product.model';
 
 @Component({
   selector: 'app-product',
-  templateUrl: './product.component.html'
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
 })
 
 export class ProductComponent implements OnChanges, OnInit, OnDestroy {
