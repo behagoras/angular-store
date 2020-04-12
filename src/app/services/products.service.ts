b603895828290659b6da2e72c7bf0f3682966288
David Behar <david@behar.io>
2020-04-12 14:18:17 -0500

    product-detail base layout & navigation

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -11,42 +11,42 @@ export class ProductsService {
       image: 'assets/images/camiseta.png',
       title: 'Camiseta',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repellat quod dolorem error temporibus delectus laboriosam vitae quae ex aperiam aut iusto unde ut asperiores, similique enim natus molestiae minima fugiat voluptates? Itaque nisi repellendus quis sit repudiandae voluptatem fugiat at, numquam magnam maxime illo veritatis doloremque aperiam rem facilis!'
     },
     {
       id: '2',
       image: 'assets/images/hoodie.png',
       title: 'Hoodie',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repellat quod dolorem error temporibus delectus laboriosam vitae quae ex aperiam aut iusto unde ut asperiores, similique enim natus molestiae minima fugiat voluptates? Itaque nisi repellendus quis sit repudiandae voluptatem fugiat at, numquam magnam maxime illo veritatis doloremque aperiam rem facilis!'
     },
     {
       id: '3',
       image: 'assets/images/mug.png',
       title: 'Mug',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repellat quod dolorem error temporibus delectus laboriosam vitae quae ex aperiam aut iusto unde ut asperiores, similique enim natus molestiae minima fugiat voluptates? Itaque nisi repellendus quis sit repudiandae voluptatem fugiat at, numquam magnam maxime illo veritatis doloremque aperiam rem facilis!'
     },
     {
       id: '4',
       image: 'assets/images/pin.png',
       title: 'Pin',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repellat quod dolorem error temporibus delectus laboriosam vitae quae ex aperiam aut iusto unde ut asperiores, similique enim natus molestiae minima fugiat voluptates? Itaque nisi repellendus quis sit repudiandae voluptatem fugiat at, numquam magnam maxime illo veritatis doloremque aperiam rem facilis!'
     },
     {
       id: '5',
       image: 'assets/images/stickers1.png',
       title: 'Stickers',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repellat quod dolorem error temporibus delectus laboriosam vitae quae ex aperiam aut iusto unde ut asperiores, similique enim natus molestiae minima fugiat voluptates? Itaque nisi repellendus quis sit repudiandae voluptatem fugiat at, numquam magnam maxime illo veritatis doloremque aperiam rem facilis!'
     },
     {
       id: '6',
       image: 'assets/images/stickers2.png',
       title: 'Stickers',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repellat quod dolorem error temporibus delectus laboriosam vitae quae ex aperiam aut iusto unde ut asperiores, similique enim natus molestiae minima fugiat voluptates? Itaque nisi repellendus quis sit repudiandae voluptatem fugiat at, numquam magnam maxime illo veritatis doloremque aperiam rem facilis!'
     },
   ];
   constructor() { }
