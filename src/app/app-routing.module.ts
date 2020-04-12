7ea1bf14fe07a01eb997e3293e9b142078d07ae7
David Behar <david@behar.io>
2020-04-12 13:45:52 -0500

    get all routing ending with .html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route, UrlSegment } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component'
 import { ContactComponent } from './contact/contact.component';
 import { TestComponent } from './test/test.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+
 const routes: Routes = [
   {
-    path: 'home',
-    redirectTo: '',
+    path: '',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: '',
+    path: 'home',
     component: HomeComponent
+  },
+  {
+    // path: '',
+    component: HomeComponent,
+    matcher: (url: UrlSegment[]) => {
+      return url.length === 1 && url[0].path.endsWith('.html') ? ({consumed: url}) : null;
+    },
   },
   {
     path: 'products',
     component: ProductsComponent
   },
+
   {
     path: 'contact',
     component: ContactComponent
