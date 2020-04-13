917941820ba8cf9df0b91644121a83675104b616
David Behar <david@behar.io>
2020-04-13 09:37:30 -0500

    cart component added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,13 +7,15 @@ import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MaterialModule } from '../material/material.module';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     ExponentialPipe,
     HighlightDirective,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CartComponent
   ],
   exports: [
     ExponentialPipe,
