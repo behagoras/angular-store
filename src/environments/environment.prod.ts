9a8efeaf693bb477d01f143a4f44e540db490c9f
David Behar <david@behar.io>
2020-04-14 10:28:02 -0500

    .gitignore fix

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  url_api: 'http://platzi-store.herokuapp.com',
-};
--- src/environments/environment.stag.ts
+++
@@ -1,5 +0,0 @@
-
-export const environment = {
-  production: false,
-  url_api: 'http://platzi-store-stag.herokuapp.com',
-};
