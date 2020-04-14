cd01e0205d5016a620049286d30bfe862dbc3ae3
David Behar <david@behar.io>
2020-04-14 09:17:19 -0500

    Observable in rxjs for price

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,27 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../../core/services/cart.service'
 import { Product } from 'src/app/product.model';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  total = 0;
+  total$: Observable<number>;
   constructor(
     private cartService: CartService,
   ) {
-    this.cartService.cart$
-    .pipe(
-      map(products => products.length)
-    )
-    .subscribe(total=>{
-      this.total = total;
-    })
-    // .subscribe((products: Product[]) => {
-    //   this.total = products.length;
-    //   // console.log(this.total);
-    // })
+    this.total$ = this.cartService.cart$
+      .pipe(map(products => products.length));
   }
 
   ngOnInit(): void {
