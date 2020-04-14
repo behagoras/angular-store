3a12eb91d907ff32ebbc2b36a1d6be48beec7a21
David Behar <david@behar.io>
2020-04-14 09:13:14 -0500

    pipe for total in cart

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../../core/services/cart.service'
 import { Product } from 'src/app/product.model';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,10 +12,17 @@ export class HeaderComponent implements OnInit {
   constructor(
     private cartService: CartService,
   ) {
-    this.cartService.cart$.subscribe((products: Product[]) => {
-      this.total = products.length;
-      // console.log(this.total);
+    this.cartService.cart$
+    .pipe(
+      map(products => products.length)
+    )
+    .subscribe(total=>{
+      this.total = total;
     })
+    // .subscribe((products: Product[]) => {
+    //   this.total = products.length;
+    //   // console.log(this.total);
+    // })
   }
 
   ngOnInit(): void {
